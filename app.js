a756e3305aa31ef16bfe1691681e9767fae45696
bfglix <resor95@gmail.com>
2026-01-01 03:52:19 +0800

    Feat: Add Particles.js background

--- app.js
+++ app.js
@@ -36,6 +36,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // We will attach this to elements after rendering is complete in renderPortfolio
     window.revealObserver = observer;
+    // Particles.js Config
+    if (typeof particlesJS !== 'undefined') {
+        particlesJS('particles-js', {
+            "particles": {
+                "number": { "value": 80, "density": { "enable": true, "value_area": 800 } },
+                "color": { "value": "#00f3ff" },
+                "shape": { "type": "circle" },
+                "opacity": { "value": 0.5, "random": false },
+                "size": { "value": 3, "random": true },
+                "line_linked": { "enable": true, "distance": 150, "color": "#00f3ff", "opacity": 0.2, "width": 1 },
+                "move": { "enable": true, "speed": 2, "direction": "none", "random": false, "straight": false, "out_mode": "out", "bounce": false }
+            },
+            "interactivity": {
+                "detect_on": "canvas",
+                "events": { "onhover": { "enable": true, "mode": "repulse" }, "onclick": { "enable": true, "mode": "push" }, "resize": true },
+                "modes": { "repulse": { "distance": 100, "duration": 0.4 }, "push": { "particles_nb": 4 } }
+            },
+            "retina_detect": true
+        });
+    }
+
 });
 
 function renderPortfolio(data) {
